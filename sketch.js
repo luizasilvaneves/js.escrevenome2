be359980d106e08cc5e40d4c40048eb8fa2d4a01
lueduardas <148785840+luizasilvaneves@users.noreply.github.com>
2023-11-06 11:50:17 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+  }
+  function draw() { 
+    stroke("blue");
+    fill("red")
+    if(mouseIsPressed);
+    rect(mouseX, mouseY, 20, 35)
+  }
